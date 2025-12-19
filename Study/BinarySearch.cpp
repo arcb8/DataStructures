03d0974578193d60d04fe4b63bf697e9123f8e7b
arcb8 <arcb8@gmail.com>
2025-12-19 20:34:00 +0200

    add triangles and binary search

--- Study/BinarySearch.cpp
+++ Study/BinarySearch.cpp
@@ -4,7 +4,7 @@ int main()
 {
     int target;
     int A[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
-    size_t size = sizeof(A) / sizeof(A[0]);
+    size_t size = sizeof(A) / sizeof(A[0]); // 14 * 4 / 4
     std::cout << "Enter the number:\n";
     std::cin >> target;
     for (int i = 0; i < size; i++) {
@@ -15,7 +15,7 @@ int main()
 
     int l = 0;
     int r = size - 1;
-    while (l < r) {
+    while (l <= r) {
         int mid = (l + r) / 2;
 
         if (A[mid] == target) {
--- Study/Triangles.cpp
+++ Study/Triangles.cpp
@@ -26,6 +26,39 @@ int main()
         }
         std::cout << "\n";
     }
+
+    // /\
+
+    n = 7;
+
+    for (int i = n/2; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (j >= n - i - 1 && i >= j) {
+                std::cout << "*";
+            }
+            else {
+                std::cout << " ";
+            }
+        }
+        std::cout << "\n";
+    }
+
+    // \/
+
+    n = 7;
+
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (j <= n - i - 1 && i <= j) {
+                std::cout << "*";
+            }
+            else {
+                std::cout << " ";
+            }
+        }
+        std::cout << "\n";
+    }
     return 0;
 }
 
